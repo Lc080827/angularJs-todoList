d1b6b83fb56b3f02d4722303c66975bbe6a6b5fd
LC080827 <741332627@qq.com>
2018-02-27 15:59:26 +0800

    todoList

--- js/controller.js
+++ js/controller.js
@@ -14,4 +14,19 @@ app.controller("todoListController",function($scope){
         $scope.todoList.push({text:$scope.text});
         $scope.text = "";
     }
+    //删除信息
+    $scope.delete = function(index){
+        $scope.todoList.splice(index,1);
+    }
+    //完成信息
+    $scope.todoList1 = [];
+    $scope.done = function(index){
+        var todo = $scope.todoList.splice(index,1)[0];
+        $scope.todoList1.push(todo);
+    }
+    //撤销完成
+    $scope.undone = function(index){
+        var todo = $scope.todoList1.splice(index,1)[0];
+        $scope.todoList.push(todo);
+    }
 });
